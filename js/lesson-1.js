7c620e93eeca0c50d7fe74a1ba25c2fd0d2d69af
Savranin-Denis <savranindenisv@gmail.com>
2026-06-12 19:44:51 +0300

    add practice lesson-1

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -28,31 +28,14 @@
 
 // // console.log(min);
 
-// switch (min) {
-//   case first:
-//     min = 15;
-//     console.log(min);
-
-//     break;
-//   case second:
-//     break;
-//   case third:
-//     break;
-//   case fourth:
-//     break;
-
-//   default:
-//     break;
-// }
-
-// if (min < 15) {
-//   alert(`${min} at 1st`);
-// } else if (min < 30) {
-//   alert(`${min} at 2st`);
-// } else if (min < 45) {
-//   alert(`${min} at 3st`);
+// if (min <= 15) {
+//   alert('Min 0 - 15');
+// } else if (min <= 30) {
+//   alert('Min 16 - 30');
+// } else if (min <= 45) {
+//   alert('Min 31 - 45');
 // } else {
-//   alert(`${min} at 4st`);
+//   alert('Min 46 - 59');
 // }
 
 // Змінна num може набувати 4 значення: '1', '2', '3' або '4'
@@ -65,25 +48,25 @@
 // "Вибачте, але ви маєте ввести значення від 1 до 4 включно".
 // Значення змінної result виведіть в консоль.
 
-// const num = prompt('enter number from 1 to 4');
+// const num = +prompt('Enter seasons number 1 - 4');
 // let result;
 
 // switch (num) {
-//   case '1':
+//   case 1:
 //     result = 'winter';
 //     break;
-//   case '2':
+//   case 2:
 //     result = 'spring';
 //     break;
-//   case '3':
+//   case 3:
 //     result = 'summer';
 //     break;
-//   case '4':
+//   case 4:
 //     result = 'autumn';
 //     break;
 
 //   default:
-//     result = 'So, What time of the year?';
+//     result = 'Sorry, enter a number from 1 to 4';
 // }
 
 // console.log(result);
@@ -103,20 +86,24 @@
 // то вивести в alert рядок "Добрий день!",
 // в іншому випадку вивести в alert рядок "Невірний пароль!"
 
-// const userLogin = prompt('Enter your login');
+// const login = prompt('Enter your login');
+// let password;
 
-// if (userLogin === 'admin') {
-//   const userPassword = prompt('Enter your password');
-//   if (userPassword === 'main') {
-//     alert(`Hello Main!`);
+// if (login === null || login.trim() === '') {
+//   alert('Cancelled');
+// } else if (login === 'admin') {
+//   password = prompt('Enter your password');
+//   if (password === 'main') {
+//     alert('Welcome admin!');
 //   } else {
-//     alert('Wrong Password');
+//     alert('Wrong password!');
 //   }
-// } else if (userLogin === '' || userLogin === null) {
-//   alert('cancelled');
 // } else {
-//   alert('Who are you???');
+//   alert('Who are you?!');
 // }
+
+// console.log(login);
+// console.log(password);
 
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -177,16 +164,113 @@
 // Корисне посилання для відображення годин та хвилин у потрібному форматі ('01' замість '1'):
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart#examples
 
-// let timeValue = prompt('Please enter number');
-// let hours = String(Math.floor(timeValue / 60)).padStart(2, '0');
-// let minutes = String(timeValue % 60).padStart(2, '0');
+// const timeValue = prompt('Enter the time value in minutes');
 
-// console.log(hours);
-// console.log(minutes);
-// console.log(`${hours}:${minutes}`);
+// if (timeValue > 0 && timeValue <= 1440) {
+//   const hours = String(parseInt(timeValue / 60)).padStart(2, '0');
+//   const minutes = String(timeValue % 60).padStart(2, '0');
 
-// 'abc'.padStart(10); // "       abc"
-// 'abc'.padStart(10, 'foo'); // "foofoofabc"
-// 'abc'.padStart(6, '123465'); // "123abc"
-// 'abc'.padStart(8, '0'); // "00000abc"
-// 'abc'.padStart(1); // "abc"
+//   console.log(`Correct time is ${hours}:${minutes}`);
+// } else {
+//   alert('Enter correct time in minutes');
+// }
+
+// Напишіть код, який запитуватиме у користувача
+// логін за допомогою prompt і виводить результат в консоль браузера
+
+// Після цього додайте перевірку введеного користувачем значення:
+// Якщо відвідувач вводить "Адмін",
+// то prompt запитує пароль (не забудьте зберігти його у змінну для подальшої перевірки).
+// Якщо нічого не ввели або натиснули Cancel,
+// Вивести в alert строку "Скасовано"
+// В іншому випадку вивести в alert рядок "Я вас не знаю"
+
+// Пароль перевіряти так:
+// Якщо введено пароль "Я головний",
+// то вивести в alert рядок "Добрий день!",
+// в іншому випадку вивести в alert рядок "Невірний пароль!"
+
+// Використайте цикл while, щоб вивести в console всі числа від 0 до 20 включно.
+
+// let min = 0;
+// const max = 20;
+
+// while (min <= max) {
+//   console.log(min);
+//   min++;
+// }
+
+// Напишіть функцію getNumbers(min, max), що приймає 2 параметри -
+// мінімальне і максимальне число відповідно.
+// Напишіть цикл всередині функції, який виводить у консоль
+// всі числа від max до min за спаданням.
+// Окрім цього, підрахуйте суму всіх парних чисел в цьому циклі
+// і поверніть її з функції.
+
+// function getNumbers(min, max) {
+//   let total = 0;
+//   while (max >= min) {
+//     console.log(max);
+//     if (max % 2 === 0) {
+//       total += max;
+//     }
+//     max--;
+//   }
+//   return total;
+// }
+
+// console.log(getNumbers(2, 20));
+
+// Напишіть функцію isAdult(age), яка  приймає число - вік користувача і повертає true,
+// якщо параметр age більше чи дорівнює 18.
+// В іншому випадку вона запитує підтвердження через confirm
+// і повертає його результат (true/false).
+
+// function isAdult(age) {
+//   if (age >= 18) {
+//     return true;
+//   } else {
+//     confirm('Are you rly adult?');
+//     return false;
+//   }
+// }
+
+// const age = +prompt('Are you rly adult?');
+// const isAdult = age => (age >= 18 ? true : false);
+// console.log(isAdult(age));
+
+// Напишіть функцію fizzBuzz(num), яка приймає число і перевіряє кожне число від 1 до num:
+// Якщо число ділитися  без остачі на 3 - виводить в консоль 'fizz',
+// якщо ділиться  без остачі на 5 - виводить в консоль 'buzz',
+// якщо ділиться  без остачі і на 3, і на 5 - виводить в консоль 'fizzbuzz'.
+
+// function fizzBuzz(num) {
+//   while (num >= 1) {
+//     if (num % 3 === 0 && num % 5 === 0) {
+//       console.log('fizzbuzz');
+//     } else if (num % 3 === 0) {
+//       console.log('fizz');
+//     } else if (num % 5 === 0) {
+//       console.log('buzz');
+//     } else {
+//       console.log(num);
+//     }
+//     num--;
+//   }
+// }
+
+// function fizzBuzz(num) {
+//   for (let i = 1; i <= num; i++) {
+//     if (i % 3 == 0 && i % 5 === 0) {
+//       console.log('fizzbuzz');
+//     } else if (i % 3 === 0) {
+//       console.log('fizz');
+//     } else if (i % 5 === 0) {
+//       console.log('buzz');
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// }
+
+fizzBuzz(20);
